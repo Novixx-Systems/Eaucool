0eed510812972634b1373d92ee57650e06e3ce79
Goomba <milan.raymakers@gmail.com>
2025-05-22 19:15:09 +0200

    remove useless function and start documenting

--- Utilities/Utils.cs
+++ Utilities/Utils.cs
@@ -17,22 +17,10 @@ namespace Eaucool.Utilities
             currentChar = 0;
         }
 
-        // I literally tried A LOT of different ways to do this, and then I found this on StackOverflow lmfaoooooooo
-        public static void SaveFile(string stringBuffer, string fname)
-        {
-            byte[] buffer = Encoding.UTF8.GetBytes(stringBuffer);
-            List<byte> bytes = new List<byte>(buffer);
-            string[] splitString = stringBuffer.Split('\n');
-            int lengthOfFourLines = splitString[0].Length + splitString[1].Length + splitString[2].Length + splitString[3].Length + 4;
-            bytes.RemoveRange(0, lengthOfFourLines);
-            int lengthOfLastLine = splitString[^2].Length + 2;
-            bytes.RemoveRange(bytes.Count - lengthOfLastLine, lengthOfLastLine);
-            buffer = bytes.ToArray();
-            FileStream file = File.Create(fname);
-            file.Write(buffer);
-            file.Close();
-        }
-        //TODO add comment what this is used for
+        /// <summary>
+        /// Replaces a word in a string with another word, but only if the word is not part of another word (full word match)
+        /// </summary>
+        /// <returns>The string with the word replaced</returns>
         public static string ReplaceWord(this string text, string word, string bywhat)
         {
             static bool IsWordChar(char c) => char.IsLetterOrDigit(c) || c == '_' || c == '$';
@@ -53,7 +41,11 @@ namespace Eaucool.Utilities
             sb.Append(text, p, text.Length - p);
             return sb.ToString();
         }
-        // Chrck if running with elevated privileges
+
+        /// <summary>
+        /// On Windows, checks if the current user is an admin
+        /// </summary>
+        /// <returns>True if the current user is an admin, false otherwise</returns>
         public static bool IsAdmin()
         {
             if (Environment.OSVersion.Platform == PlatformID.Win32NT)
@@ -67,6 +59,13 @@ namespace Eaucool.Utilities
                 return true; // Linux and MacOS don't have the same concept of admin privileges AFAIK
             }
         }
+
+        /// <summary>
+        /// Gets the string from an array of strings, and replaces all Eaucool variables with their values
+        /// </summary>
+        /// <param name="args">The array of strings to get the string from</param>
+        /// <param name="arg">The index of the string to get</param>
+        /// <returns>The string from the array of strings</returns>
         public static string GetString(string[] args, int arg = 0, bool noSpace = true)
         {
             string returnValue;
@@ -87,6 +86,11 @@ namespace Eaucool.Utilities
             return returnValue;
         }
 
+        /// <summary>
+        /// Gets the lines after the given line number
+        /// </summary>
+        /// <param name="line">The line number to get the lines after</param>
+        /// <returns>An array of strings containing the lines after the given line number</returns>
         internal static string[] GetLinesAfter(int line)
         {
             string[] lines = Program.currentFileCode.Split('\n');
